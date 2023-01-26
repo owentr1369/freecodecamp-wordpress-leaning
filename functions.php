0947a1a36afe26bc8d60641e4b9be68be46c2167
TranTam <tamtt1@firegroup.io>
2023-01-26 12:54:41 +0700

    Init commit

---
+++ functions.php
@@ -0,0 +1,42 @@
+<?php
+
+function learningwp_theme_support()
+{
+    // Add dynamic title tag support
+    add_theme_support('title-tag');
+    add_theme_support('custom-logo');
+    add_theme_support('post-thumbnails');
+}
+
+
+add_action('after_setup_theme', 'learningwp_theme_support');
+
+function learningwp_menu()
+{
+    $locations = array(
+        'primary' => 'Desktop Primary Left Sidebar',
+        'footer' => 'Footer Menu Items'
+    );
+
+    register_nav_menus($locations);
+}
+
+add_action('init', 'learningwp_menu');
+
+function learningwp_register_styles()
+{
+    $version = wp_get_theme()->get("Version");
+    wp_enqueue_style('learningwp-fontawesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css', '5.13.0', 'all');
+    wp_enqueue_style('learningwp-bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css', array(), '4.4.1', 'all');
+    wp_enqueue_style('learningwp-style', get_template_directory_uri() . "/style.css", array(), $version, 'all');
+}
+add_action('wp_enqueue_scripts', 'learningwp_register_styles');
+
+function learningwp_register_scripts()
+{
+    wp_enqueue_script('learningwp-jquery', 'https://code.jquery.com/jquery-3.4.1.slim.min.js', array(), '3.4.1', true);
+    wp_enqueue_script('learningwp-popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js', array(), '1.16.0', true);
+    wp_enqueue_script('learningwp-bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js', array(), '4.4.1', true);
+    wp_enqueue_script('learningwp-main', get_template_directory_uri() . "/blog-site-template/js/main.js", array(), '1.0', true);
+}
+add_action('wp_enqueue_scripts', 'learningwp_register_scripts');
