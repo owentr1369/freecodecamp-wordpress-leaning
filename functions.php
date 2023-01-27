cd93c46f7417aeb01d9a60b58d6481b7bad82980
TranTam <tamtt1@firegroup.io>
2023-01-28 00:07:37 +0700

    add searching

--- functions.php
+++ functions.php
@@ -44,15 +44,25 @@ add_action('wp_enqueue_scripts', 'learningwp_register_scripts');
 
 function learningwp_widget_areas()
 {
+    register_sidebar(
+        array(
+            'before_title' => '',
+            'after_title' => '',
+            'before_widget' => '<ul class="social-list list-inline py-3 mx-auto">',
+            'after_widget' => '</ul>',
+            'name' => 'Sidebar Area',
+            'id' => 'sidebar-1',
+            'description' => 'Sidebar Widget Area',
+        )
+    );
     register_sidebar(array(
         'before_title' => '',
         'after_title' => '',
-        'before_widget' => '<ul class="social-list list-inline py-3 mx-auto">',
-        'after_widget' => '</ul>',
-    ), array(
-        'name' => 'Sidebar Area',
-        'id' => 'sidebar-1',
-        'description' => 'Sidebar Widget Area'
+        'before_widget' => '',
+        'after_widget' => '',
+        'name' => 'Footer Area',
+        'id' => 'footer-1',
+        'description' => 'Footer Widget Area',
     ));
 }
 
